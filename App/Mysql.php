7a6c51e7432c921801341b94509dc4f000ef9769
gregory <gregory.mostacci@ynov.com>
2020-03-24 18:27:44 +0100

    Change SQL Class in Singleton

--- App/Mysql.php
+++ App/Mysql.php
@@ -5,22 +5,44 @@ namespace App;
 
 use PDO;
 
-class Mysql{
+class Mysql extends PDO{
 
     private $host = "localhost";
     private $dbname = "webproject";
     private $user = "root";
     private $pass = "";
 
-    public function dbConnect(): PDO
+    private static $instance = null;
+    private $PDOInstance = null;
+
+    public function __construct()
     {
         try {
-            $db = new PDO("mysql:host=$this->host;dbname=$this->dbname;charset=utf8", $this->user, "$this->pass");
-            return $db;
+                $this->PDOInstance = new PDO("mysql:host=$this->host;dbname=$this->dbname;charset=utf8", $this->user, $this->pass);
         } catch (\PDOException $e) {
             exit("Une erreur est survenue. Erreur #1");
         }
     }
 
+    public static function getInstance() {
+
+        if(is_null(self::$instance)) {
+            self::$instance = new Mysql();
+        }
+
+        return self::$instance;
+    }
+    public function query($query)
+    {
+        return $this->PDOInstance->query($query);
+    }
+    public function prepare($query, $options = NULL)
+    {
+        return $this->PDOInstance->prepare($query);
+    }
+    public function execute($query)
+    {
+        return $this->PDOInstance->execute($query);
+    }
 
 }
