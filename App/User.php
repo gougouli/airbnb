83a434eb3c6a6ca3db283d6de74aebc93f1f11ce
gregory <gregory.mostacci@ynov.com>
2020-03-27 10:52:04 +0100

    Add exist method in User class

--- App/User.php
+++ App/User.php
@@ -24,4 +24,31 @@ class User
         $stmt = $db->prepare("UPDATE users SET isActive = 1 WHERE id = ?");
         $stmt->execute([$id]);
     }
+
+    public function exist($email, $pass, $hash=1): int{
+        $db = Mysql::getInstance();
+        if($hash){
+            $pass = sha1($pass);
+        }
+        $req = $db->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
+        $req->execute([$email, $pass]);
+        if($req->rowCount() == 1){
+            $req = $req->fetch();
+            if($req['isActive'] == 1){
+                return $req['id'];
+            }else{
+                if($req['isActive'] == 3){
+                    $_SESSION['errors'][] = "Votre compte est entrain de subir un changement de mot de passe. Contactez un administrateur si vous n'etes pas à l'origine de cette requete.";
+                }elseif($req['isActive'] == 0){
+                    $_SESSION['errors'][] =  "Votre compte a été bannis.";
+                }else{
+                    $_SESSION['errors'][] =  "Votre compte n'est pas activé. Vérifiez vos emails.";
+                }
+            }
+        }else{
+            $_SESSION['errors'][] =  "Votre email ou votre mot de passe est incorrect.";
+
+        }
+        return 0;
+    }g
 }
--- App/log.php
+++ App/log.php
@@ -4,32 +4,7 @@ use App\Mysql;
 use App\Session;
 use App\Token;
 
-function exist($email, $pass, $hash=1): int{
-    $db = Mysql::getInstance();
-    if($hash){
-        $pass = sha1($pass);
-    }
-    $req = $db->prepare("SELECT * FROM users WHERE email = ? AND password = ?");
-    $req->execute([$email, $pass]);
-    if($req->rowCount() == 1){
-        $req = $req->fetch();
-        if($req['isActive'] == 1){
-            return $req['id'];
-        }else{
-            if($req['isActive'] == 3){
-                $_SESSION['errors'][] = "Votre compte est entrain de subir un changement de mot de passe. Contactez un administrateur si vous n'etes pas à l'origine de cette requete.";
-            }elseif($req['isActive'] == 0){
-                $_SESSION['errors'][] =  "Votre compte a été bannis.";
-            }else{
-                $_SESSION['errors'][] =  "Votre compte n'est pas activé. Vérifiez vos emails.";
-            }
-        }
-    }else{
-        $_SESSION['errors'][] =  "Votre email ou votre mot de passe est incorrect.";
 
-    }
-    return 0;
-}
 
 
 
